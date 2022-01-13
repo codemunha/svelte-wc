df077333edb798e5f3bec4305b22bedb7bd4b11b
Sutin <poly.dev2018@gmail.com>
2022-01-13 11:01:54 +0700

    using css custom properties

--- src/lib/web_components/Buttons/Button.svelte
+++ src/lib/web_components/Buttons/Button.svelte
@@ -1,9 +1,18 @@
 <svelte:options tag="sv-button" />
 
 <script>
+  export let label = "Button";
 </script>
 
-<h1>Button</h1>
+<button>{label}</button>
 
 <style>
+  button {
+    padding: var(--padding, 0.25em);
+    margin: var(--margin, 0.25em);
+    color: var(--color);
+    background-color: var(--background-color);
+    font-size: var(--font-size);
+    border: var(--border, 1px solid #000);
+  }
 </style>
