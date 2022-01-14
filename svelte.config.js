8d9d7f6a52a3364f33e446a6ee5b97b13d754833
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-01-14 22:18:35 +0700

    update

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,4 @@ import sveltePreprocess from "svelte-preprocess";
 
 export default {
   preprocess: sveltePreprocess(),
-  plugins: [
-    svelte({
-      compilerOptions: {
-        customElement: true,
-      },
-    }),
-  ],
 };
