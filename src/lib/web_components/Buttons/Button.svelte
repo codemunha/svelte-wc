dbe83881fc0349a0627daf1403200d2fd8643cd9
Sutin <poly.dev2018@gmail.com>
2022-01-14 20:05:36 +0700

    added custom css vars

--- src/lib/web_components/Buttons/Button.svelte
+++ src/lib/web_components/Buttons/Button.svelte
@@ -1,18 +1,35 @@
-<svelte:options tag="sv-button" />
+<svelte:options tag="s-button" />
 
 <script>
+  import cssVars from "svelte-css-vars";
   export let label = "Button";
+  export let padding = "1px";
+  export let margin = "1px";
+  export let color;
+  export let size;
+
+  $: styleVars = {
+    padding,
+    margin,
+    color,
+    "font-size": size,
+  };
 </script>
 
-<button>{label}</button>
+{#if $$slots.content}
+  <slot name="content" />
+{:else}
+  <div {...$$props} use:cssVars={styleVars}>{label}</div>
+{/if}
 
 <style>
-  button {
+  div {
     padding: var(--padding, 0.25em);
     margin: var(--margin, 0.25em);
-    color: var(--color);
-    background-color: var(--background-color);
-    font-size: var(--font-size);
-    border: var(--border, 1px solid #000);
+    color: var(--color, black);
+    background-color: var(--background-color, transparent);
+    font-size: var(--font-size, 14px);
+    cursor: var(--cursor, pointer);
+    /* border: var(--border, 1px solid #000); */
   }
 </style>
